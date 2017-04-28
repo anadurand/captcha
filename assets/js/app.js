90c36dca097ee64cb217c976d7b057104ac845f3
Ana Durand <ana.dc1004@gmail.com>
2017-04-28 13:40:04 -0500

    empezando a refactorizar

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,7 +14,6 @@ createCaptcha();
 function createCaptcha(){
   var alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   var number = ["0","1","2","3","4","5","6","7","8","9","'"];
-  for (i=0;i<6;i++){
      var a = alpha[Math.floor(Math.random() * alpha.length)];
      var b = number[Math.floor(Math.random() * number.length)];
      var c = alpha[Math.floor(Math.random() * alpha.length)];
@@ -22,12 +21,10 @@ function createCaptcha(){
      var e = alpha[Math.floor(Math.random() * alpha.length)];
      var f = number[Math.floor(Math.random() * number.length)];
      var g = alpha[Math.floor(Math.random() * alpha.length)];
-  }
   var captchaCode = a + b + c + d + e + f + g;
   document.getElementById("captchaCode").value = captchaCode;
   var code = document.getElementById('captchaCode');
   var answerCode = document.getElementById('answer');
-
   code.style.color = changeColor();
   answerCode.style.color = changeColor();
 
